88ca3406f80e6553e3aad785f29e3699d1fbf5f1
Sagnik Das <mailsagnikdas53@gmail.com>
2026-06-29 18:07:47 +0530

    refactor: eagerly load OCR regions to prevent lazy proxy initialization issues in InternalJobController

--- backend/src/main/java/com/manga/library/model/OcrRegion.java
+++ backend/src/main/java/com/manga/library/model/OcrRegion.java
@@ -13,6 +13,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@com.fasterxml.jackson.annotation.JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class OcrRegion {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
